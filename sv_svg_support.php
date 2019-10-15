94ba0f61330c89946ed1a6a595b0aea9ee8eb651
Matthias Bathke <matthias-bathke@straightvisions.com>
2019-10-15 13:55:57 +0200

    MIGRATE TO SV COMPANION PLUGIN

--- sv_svg_support.php
+++ sv_svg_support.php
@@ -1,5 +1,5 @@
 <?php
-namespace sv_100;
+namespace sv100_companion;
 
 /**
  * @version         1.00
@@ -11,44 +11,46 @@ namespace sv_100;
  * @license			See license.txt or https://straightvisions.com
  */
 
-class sv_svg_support extends init {
-	public function __construct() {
-
-	}
-
+class sv_svg_support extends modules {
 	public function init() {
-		// Translates the module
-		load_theme_textdomain( $this->get_module_name(), $this->get_path( 'languages' ) );
-
 		// Module Info
-		$this->set_module_title( 'SV SVG Support' );
-		$this->set_module_desc( __( 'This module loads styles of plugin <a href="https://de.wordpress.org/plugins/svg-support/" target="_blank">SVG Support</a> inline to improve Pagespeed.', $this->get_module_name() ) );
+		$this->set_section_title( __('SVG Support','sv100_companion') );
+		$this->set_section_desc( sprintf(__( 'This module loads styles of plugin %sSVG Support%s inline to improve Pagespeed.', 'sv100_companion' ),'<a href="https://de.wordpress.org/plugins/svg-support/" target="_blank">','</a>' ))
+		->set_section_type( 'settings' );
+		
+		$this->get_root()->add_section($this);
+		
+		$this->load_settings();
 		
 		// WP Styles
 		add_action( 'wp_print_styles', array($this, 'wp_print_styles'), 100 );
 		add_action('wp', array($this, 'load'));
-		
-		$this->register_scripts();
 	}
 	public function wp_print_styles() {
 		if(defined('BODHI_SVGS_PLUGIN_PATH')) {
-			// Gutenberg: load Styles inline for Pagespeed purposes
+			// load Styles inline for Pagespeed purposes
 			wp_dequeue_style('bodhi-svgs-attachment');
 		}
 	}
 	protected function register_scripts(): sv_svg_support {
-		$this->scripts_queue['bodhi-svgs-attachment'] =
-			static::$scripts->create( $this )
-							->set_ID( 'bodhi-svgs-attachment' )
+		$this->get_script('bodhi-svgs-attachment')
 							->set_path( BODHI_SVGS_PLUGIN_PATH . 'css/svgs-attachment.css', true)
 							->set_inline( true );
 		
 		return $this;
 	}
 	public function load() : sv_svg_support{
-		if(defined('BODHI_SVGS_PLUGIN_PATH')) {
-			$this->scripts_queue['bodhi-svgs-attachment']->set_is_enqueued( true );
+		if(defined('BODHI_SVGS_PLUGIN_PATH') && $this->get_setting('load_inline')->run_type()->get_data()) {
+			$this->register_scripts();
+			$this->get_script('bodhi-svgs-attachment')->set_is_enqueued( true );
 		}
+		
+		return $this;
+	}
+	public function load_settings(): sv_svg_support{
+		$this->get_setting('load_inline')
+			 ->set_title( __( 'Load Styles inline.', 'sv100_companion' ) )
+			 ->load_type( 'checkbox' );
 		
 		return $this;
 	}
